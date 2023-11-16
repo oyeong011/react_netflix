6aea776fc4051ea937177a4952810f4ca2c76c65
oyeong011 <oyeong011@gmail.com>
2023-11-16 13:03:32 +0900

    Feat : 넷플릭스 프로젝트 시작

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+const { default: axios } = require("axios");
+
+const instance = axios.create({
+    baseURL : "https://api.themoviedb.org/3",
+    params : {
+        api_key : "4036ea10d60c0a0652536f8d94f36a3b",
+        language : "ko-KR"
+    }
+});
+
+export default instance;
